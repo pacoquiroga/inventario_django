ff61e576760d0349ff22bbe1b8852010afd037bd
Francisco Quiroga <fsquiroga2@espe.edu.ec>
2024-05-29 18:32:57 -0500

    first commit

---
+++ productos/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from .models import Producto
+
+productos = []
+
+def listar_productos(request):
+    productos = Producto.objects.all()
+    return render(request, 'listar.html', {'productos': productos})
